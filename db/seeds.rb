08ea99f1ad5b46ada0855dfd32e22232e362d129
benoit bessiere <benoit.bessiere@gmail.com>
2019-07-26 10:52:09 +0200

    add some seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Task.destroy_all
+
+task = Task.create! title: 'braquemar', details: 'Attention le loup est sorti de la forêt', completed: true
+task = Task.create! title: 'pharmacie', details: 'Besoin de kardegic et Modiodale', completed: false
+task = Task.create! title: 'cardiologue', details: 'rendre rdv', completed: false
+
